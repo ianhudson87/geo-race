aefecf15bc6dcf86f3529b1140f2abf42a7a075e
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-05-18 17:14:28 -0500

    finished most of backend game logic

--- client/js/helper.js
+++ client/js/helper.js
@@ -1,10 +1,34 @@
 var socket = io.connect();
 
+//////////////////////////////////
+// GAME SETTINGS
+const tickrate = 0.5
+const ticktime = 1000/tickrate
+const canvas_width = 640
+const canvas_height = 480
+
+//////////////////////////////////
+// GAME DATA
+var num_racers = 0 // will change when game starts
+var game_state = {}
+
+
+//////////////////////////////////
+// canvas
 var game_canvas = document.getElementById("game_canvas")
+//game_canvas.style.width = canvas_width.toString().concat("px")
+//game_canvas.style.height = canvas_height.toString().concat("px")
 var ctx = game_canvas.getContext("2d");
+
+var racer_spacing = 0 // will change when game starts. vertical spacing in px of the racers
+var racer_size = 10 // size of racer in px
+
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 150, 75);
+ctx.fillStyle = "#0000FF";
+ctx.fillRect(100, 100, 10, 10);
 
+/////////////////////////////////
 // create room handler
 $("#create_btn").click(()=>{
     socket.emit("create_room", {
@@ -55,6 +79,8 @@ $("#start_game_btn").click(() => {
 // start game response handler
 socket.on("start_game_res", (data)=>{
     if(data.success){
+        num_racers = data.num_racers
+        racer_spacing = canvas_height/(num_racers+1)
         alert(data.msg)
     }
     else{
@@ -71,11 +97,55 @@ socket.on("update_users_display", (data)=>{
     $("#users_display").html(users_text)
 })
 
-///// Helper functions
+// update game data handler
+socket.on("update_game_state", (data)=>{
+    game_state = data.game_state
+    console.log(data.game_state)
+})
+
+// update display
+var game_display = setInterval(update_display, ticktime);
+
+/////////////////////////////
+// Helper functions
 function hide_join_create(){
     $("#join_create_div").css("display", "none")
 }
 
 function show_game(){
     $("#game_div").css("display", "block")
+}
+
+function update_display(){
+    // clear canvas
+    console.log(canvas_width)
+    //ctx.clearRect(0, 0, canvas_width, canvas_height);
+
+    let bot_index = 0
+    let human_index = 0
+
+    for(let i=0; i<num_racers; i++){
+        if(game_state.player_race_positions.includes(i)){
+            // i is at position of human
+
+            let race_position=game_state.player_positions[human_index]
+            ctx.fillStyle = "#00FF00";
+            ctx.fillRect(race_position+0.5, racer_spacing*(i+0.5), racer_size, racer_size);
+            ctx.fillText(i.toString(), race_position+0.5, racer_spacing*(i+0.5));
+
+
+            human_index++
+        }
+        else{
+            // i is at position of bot
+
+            let race_position=game_state.bot_positions[bot_index]
+            ctx.fillStyle = "#00FF00";
+            ctx.fillRect(race_position+0.5, racer_spacing*(i+0.5), racer_size, racer_size);
+            ctx.fillText(i.toString(), race_position+0.5, racer_spacing*(i+0.5));
+
+            bot_index++
+        }
+    }
+
 }
--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var io = require('socket.io')(http);
 var path = require('path');
 var port = process.env.PORT || 3000;
 
+// global variables
+const tickrate = 3
+const ticktime = 1000/tickrate
+const num_racers = 10 // number of bots+humans
+const bot_movement_toggle_chance = 0.1 // chance that bot changes walking->stop or vice versa each tick
+const win_position = 100 // position that is the finish line
+
 
 // serve up all client files
 var htmlPath = path.join(__dirname, 'client');
@@ -17,7 +24,7 @@ app.use(express.static(htmlPath));
 
 // object of objects
 // first object is all the rooms
-// second object is password and users
+// second object is password, users and other stuff
 /*
 rooms_obj = {
   room1: {
@@ -31,7 +38,15 @@ rooms_obj = {
     password: "pass2",
     users: ["user3", "user4"],
     socketids: [user3id, user4id],
-    game_started: true
+    game_started: true,
+    game_state: {
+        player_positions: [player1pos, player2pos], // positions along their path
+        player_movement_state: [0, 2] // 0=stop, 1=walk, 2=run
+        player_crosshair: [p1crosshairpos, p2crosshairpos],
+        player_race_positions: [], // which player are they in the race
+        bot_positions: [bot1pos, bot2pos, ..., bot(n-players)pos],
+        bot_movement_state: [1, 0, 1, ...] // 0=stop, 1=walk
+    }
   }
 }
 */
@@ -134,34 +149,143 @@ io.on('connection', function(socket){
     socket.on("start_game", ()=>{
 
         // find room the user is in
-        for(let room in rooms_obj){
-            // room is initially a string
-            room = rooms_obj[room]
+        for(let room_name in rooms_obj){
+            // room_name is a string (key)
+            room = rooms_obj[room_name]
 
             console.log(room.socketids)
 
             if(room.socketids.includes(socket.id)){
                 // user is in this room
-                if(room.socketids.length > 1){
-                    room.game_started = true
 
-                    socket.emit("start_game_res", {
-                        success: true,
-                        msg: "game started"
-                    })
-
-                }
-                else{
+                if(room.game_started){
+                    // game is already started
                     socket.emit("start_game_res", {
                         success: false,
-                        msg: "game not started, not enough people"
+                        msg: "game not started, game already started"
                     })
                 }
+                else{
+                    if(room.socketids.length > 1){
+                        // enough players to start game
+                        room.game_started = true
+
+                        room.game_state = initiate_game(room.socketids.length)
+    
+                        io.sockets.in(room_name).emit("start_game_res", {
+                            success: true,
+                            msg: "game started",
+                            num_racers: num_racers
+                        })
+                        console.log(room.game_state)
+                    }
+                    else{
+                        socket.emit("start_game_res", {
+                            success: false,
+                            msg: "game not started, not enough people"
+                        })
+                    }
+                }
+                break;
             }
         }
     })
+
+    // GAME ACTIONS
+
 });
 
+// GAME LOOP
+var game = setInterval(game_loop, ticktime);
+
+function game_loop() {
+    // go through each of the games and determine if they are started
+    // if they are started, then update the values in them
+    for(let room_name in rooms_obj){
+        // room_name is a string (key)
+        room = rooms_obj[room_name]
+        if(room.game_started){
+            // game is in progress
+
+            let game_state = room['game_state']
+
+            // update player positions
+            game_state['player_positions'] = game_state['player_positions'].map((val, index)=>{
+                let movement_state = game_state.player_movement_state[index]
+                return val + movement_state
+            })
+
+            // update bot positions
+            game_state['bot_positions'] = game_state['bot_positions'].map((val, index)=>{
+                let movement_state = game_state.bot_movement_state[index]
+                return val + movement_state
+            })
+
+            // randomize bot movement states
+            game_state['bot_movement_state'] = game_state['bot_movement_state'].map((val)=>{
+                if(bernoulli(bot_movement_toggle_chance)){
+                    // flip bot movement state
+                    console.log('here')
+                    return (val+1)%2
+                }
+                return val
+            })
+
+            // tell client to update game data
+            io.sockets.in(room_name).emit('update_game_state', {
+                game_state: game_state
+            });
+
+            // check if win condition is met
+            // game_state['player_positions'].some((val, index) => {
+            //     if(val >= )
+            // })
+            console.log(game_state)
+        }
+    }
+}
+
+
+// HELPER FUNCTION
+function initiate_game(num_players){
+    // returns object that is a randomized game_state
+    let game_state = {
+        player_positions: Array(num_players).fill(0),
+        player_movement_state: Array(num_players).fill(0),
+        player_crosshair: sample_no_replacement(num_racers, num_players),
+        player_race_positions: sample_no_replacement(num_racers, num_players),
+        bot_positions: Array(num_racers - num_players).fill(0),
+        bot_movement_state: Array(num_racers - num_players).fill(0),
+    }
+
+    return game_state
+}
+
+function sample_no_replacement(sample_size, n){
+    // returns array of n numbers that are sampled from 0-(sample_size-1) w/o replacement
+    let bucket = []
+    let output = []
+
+    for (let i=0; i<sample_size; i++) {
+        bucket.push(i)
+    }
+
+    for (let i=0; i<n; i++) {
+        let randomIndex = Math.floor(Math.random()*bucket.length)
+        output.push(bucket[randomIndex])
+        bucket.splice(randomIndex, 1)
+    }
+
+    return output
+}
+
+function bernoulli(p){
+    // returns true with probability p
+    return (Math.random() < p)
+}
+
+
+
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+  console.log('listening on *:' + port)
 });
